c090f8f92049688ed8bc49c9987e917d66016564
Santhosh <santhoshaarthi1999@gmail.com>
2023-04-05 06:24:54 -0700

    RRD

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation  } from 'react-router-dom'
 
 function Home() {
   const location = useLocation()
+  // const history = useHistory ()
   return (
     <>
       <h1>
         Home
       </h1>
+      {/* <button onClick={()=>history.push('/contact')}>Redirect To Contact Page</button> */}
       <h1>
         Hello {location.state}
       </h1>
